1122529c89e10c4d0bd0f28be6c0f97686677873
rccyx <oss@rccyx.com>
2025-10-12 17:49:53 +0100

    fix: the views count

--- packages/api/src/transports/v1/functions/post/delete-view-window.ts
+++ packages/api/src/transports/v1/functions/post/delete-view-window.ts
@@ -1,4 +1,3 @@
-import { db } from "@ashgw/db";
 import { logger } from "@ashgw/logger";
 import { monitor } from "@ashgw/monitor";
 import type { PostViewWindowDeleteResponses } from "../../models";
@@ -9,23 +8,11 @@ const retainDays = 2;
 export async function deleteViewWindow(): Promise<PostViewWindowDeleteResponses> {
   // compute cutoff per function run
   const cutoff = new Date(Date.now() - 1000 * 60 * 60 * 24 * retainDays);
-  logger.info("Cleaning up the post view window...", {
-    cutoffDate: cutoff.toISOString(),
-  });
 
   try {
-    await new ViewService().purgeViewWindowWithCutoff({ cutoff });
-
-    if (deleted.count > 0) {
-      logger.info("View window records purged successfully!", {
-        deleted: deleted.count,
-        cutoff: cutoff.toISOString(),
-      });
-    } else {
-      logger.info("No record to purge, view window is clean", {
-        cutoff: cutoff.toISOString(),
-      });
-    }
+    await new ViewService().purgeViewWindowWithCutoff({
+      cutoff,
+    });
 
     return {
       status: 204,
--- packages/core/src/services/view/index.ts
+++ packages/core/src/services/view/index.ts
@@ -71,9 +71,23 @@ export class ViewService {
   public async purgeViewWindowWithCutoff({
     cutoff,
   }: ViewWindowPurgeWithCutoffDto): Promise<ViewWindowPurgeWithCutoffRo> {
+    logger.info("Cleaning up the view window with: ", {
+      cutoffDate: cutoff.toISOString(),
+    });
     const deleted = await db.postViewWindow.deleteMany({
       where: { bucketStart: { lt: cutoff } },
     });
-    return { deletedCount: deleted.count };
+    const deletedCount = deleted.count;
+    if (deletedCount > 0) {
+      logger.info("View window records purged successfully!", {
+        deleted: deletedCount,
+        cutoff: cutoff.toISOString(),
+      });
+    } else {
+      logger.info("No record to purge, view window is clean", {
+        cutoff: cutoff.toISOString(),
+      });
+    }
+    return { deletedCount };
   }
 }
