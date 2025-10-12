b5d805323334ba3b0c146dd5c4a4bf1308f92a72
rccyx <oss@rccyx.com>
2025-10-12 13:33:10 +0100

    chore: update api config

--- packages/api/eslint.config.js
+++ packages/api/eslint.config.js
@@ -1,5 +1,4 @@
 import baseConfig from "@ashgw/eslint-config/base";
-import nextjsConfig from "@ashgw/eslint-config/nextjs";
 import reactConfig from "@ashgw/eslint-config/react";
 import restrictEnvAccess from "@ashgw/eslint-config/restricted-env";
 
@@ -10,6 +9,5 @@ export default [
   },
   ...baseConfig,
   ...reactConfig,
-  ...nextjsConfig,
   ...restrictEnvAccess,
 ];
