30ce262bec63d662b4e42b2e3eac929cdd2a421f
rccyx <oss@rccyx.com>
2025-10-12 14:35:42 +0100

    chore: start with fresh package exports, no more fuckery

--- packages/api/eslint.config.js
+++ packages/api/eslint.config.js
@@ -1,13 +1,5 @@
 import baseConfig from "@ashgw/eslint-config/base";
-import reactConfig from "@ashgw/eslint-config/react";
 import restrictEnvAccess from "@ashgw/eslint-config/restricted-env";
 
 /** @type {import('typescript-eslint').Config} */
-export default [
-  {
-    ignores: [".next/**"],
-  },
-  ...baseConfig,
-  ...reactConfig,
-  ...restrictEnvAccess,
-];
+export default [...baseConfig, ...restrictEnvAccess];
--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -1,17 +1,6 @@
-import path from "path";
-import { defineProject, mergeConfig } from "vitest/config.js";
+import { defineProject, mergeConfig } from "vitest/config";
 
 // importing using @ashgw/vitest-config keep erroring out, and it's annoying af!
-import { baseConfig } from "../../tooling/vitest";
+import { baseConfig } from "./../../tooling/vitest";
 
-export default mergeConfig(
-  baseConfig,
-  defineProject({
-    resolve: {
-      alias: {
-        // @see https://stackoverflow.com/questions/73022020/vitest-not-recognizing-absolute-import
-        "~": path.resolve(__dirname, "./src"),
-      },
-    },
-  }),
-);
+export default mergeConfig(baseConfig, defineProject({}));
