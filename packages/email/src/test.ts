0ee9b27325d2b1c2496469f13e19b40c24e6cd8a
rccyx <oss@rccyx.com>
2025-10-21 22:34:34 +0100

    add tester matchers

--- packages/email/src/test.ts
+++ packages/email/src/test.ts
@@ -3,11 +3,28 @@ import { send } from "./index";
 import { logger } from "@ashgw/logger";
 
 async function main(): Promise<void> {
-  await send.auth.verifyEmail({
-    to: env.PERSONAL_EMAIL,
-    verifyUrl: `https://example.com/verify?token=example`,
-    userName: "John Doe",
-  });
+  await send.auth
+    .verifyEmail({
+      to: env.PERSONAL_EMAIL,
+      verifyUrl: `https://example.com/verify?token=example`,
+      userName: "John Doe",
+    })
+    .then((r) =>
+      r.match({
+        ok: (v) => logger.info("Sent verify email", { id: v.id }),
+        err: {
+          EmailClientResponseMissingId: (e) => {
+            logger.error("Failed to send verify email", { error: e });
+          },
+          EmailClientSendingFailure: (e) => {
+            logger.error("Failed to send verify email", { error: e });
+          },
+          VerifyEmailTemplateRenderingFailure: (e) => {
+            logger.error("Failed to send verify email", { error: e });
+          },
+        },
+      }),
+    );
   logger.info("Sent verify email");
 }
 
