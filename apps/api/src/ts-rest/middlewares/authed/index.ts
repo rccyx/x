bd08d36d277267f9803baa019c3d2c63ad19690f
rccyx <oss@rccyx.com>
2025-10-28 00:26:12 +0100

    update kit

--- apps/api/src/ts-rest/middlewares/authed/index.ts
+++ apps/api/src/ts-rest/middlewares/authed/index.ts
@@ -1,14 +1,17 @@
 import { env } from "@ashgw/env";
 import type { EmptyObject } from "ts-roids";
-import { middlewareResponse, middlewareFn } from "ts-rest-kit/core";
+import { middlewareFn } from "ts-rest-kit/core";
 import type { GlobalContext } from "../../context";
 
 export function authed() {
   return middlewareFn<GlobalContext, EmptyObject>((req, _res) => {
     if (req.headers.get("x-api-token") !== env.X_API_TOKEN) {
-      return middlewareResponse.errors.unauthorized({
-        message: "Invalid token. You cannot perform this action",
-      });
+      return {
+        status: 401,
+        body: {
+          message: "Invalid token. You cannot perform this action",
+        },
+      } as const;
     }
   });
 }
