cb2e41601267da56fbd2f4b369dc9e0739632511
rccyx <oss@rccyx.com>
2025-10-28 00:53:44 +0100

    no fail safe

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -1,17 +1,25 @@
 import path from "path";
-import { defineProject, mergeConfig } from "vitest/config.js";
+import { defineProject, mergeConfig } from "vitest/config";
 
 // importing using @ashgw/vitest-config keep erroring out, and it's annoying af!
-import { baseConfig } from "../../tooling/vitest";
+import { baseConfig } from "./../../tooling/vitest";
 
 export default mergeConfig(
   baseConfig,
   defineProject({
+    test: {
+      deps: {
+        inline: [/server-only/], // the server-only package errors out when we're in a broswer env, mock it.
+      },
+      mockReset: false,
+      environment: "node",
+      setupFiles: ["./test/setup.ts"],
+      testTimeout: 15000,
+    },
     resolve: {
       alias: {
         // @see https://stackoverflow.com/questions/73022020/vitest-not-recognizing-absolute-import
         "~": path.resolve(__dirname, "./src"),
-        "~/lib": path.resolve(__dirname, "./src/lib/"),
       },
     },
   }),
---
+++ apps/blog/test/unit/test.test.ts
@@ -0,0 +1,8 @@
+import { logger } from "@ashgw/logger";
+import { describe, it } from "vitest";
+
+describe("test", () => {
+  it("should work", () => {
+    logger.debug("Tester");
+  });
+});
