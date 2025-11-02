5e44b11a6746b999c55c3cc2b3a5f05d9173ec63
rccyx <oss@rccyx.com>
2025-11-02 23:07:39 +0100

    update const

--- packages/constants/index.ts
+++ packages/constants/index.ts
@@ -1,9 +1,11 @@
-export const defaultEmail = "no-reply@notify.rccyx.com"; // TODO: change this to no reply defualt or some?
-export const defaultEmailFrom = `rccyx[bot] <${defaultEmail}>`;
-export const ossEmail = "oss@rccyx.com";
-export const repoSource = "https://github.com/rccyx/x";
-export const creator = "@rccyx";
-export const siteName = "Rccyx";
+export const email = {
+  personal: "me@rccyx.com",
+  oss: "oss@rccyx.com",
+  noReply: {
+    adress: "no-reply@notify.rccyx.com",
+    from: "rccyx[bot] <no-reply@notify.rccyx.com>",
+  },
+};
 
 export const links = {
   gitHub: {
@@ -20,3 +22,7 @@ export const gpg = {
   publicUrl: "https://github.com/rccyx.gpg",
   id: "79821E0224D34EC4969FF6A8E5168EE090AE80D0",
 } as const;
+
+export const repoSource = "https://github.com/rccyx/x";
+export const creator = "@rccyx";
+export const siteName = "Rccyx";
