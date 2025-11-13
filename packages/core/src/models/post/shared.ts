ecd1eb635b84936d0c9d4c33733fd0388ff369ff
rccyx <oss@rccyx.com>
2025-11-13 23:43:13 +0100

    update schemas

--- packages/core/src/models/post/shared.ts
+++ packages/core/src/models/post/shared.ts
@@ -7,8 +7,18 @@ export enum PostCategoryEnum {
 }
 
 // Schemas
-export const mdxText = z.string().min(10).max(30000);
-export const summary = z.string().min(10).max(90);
+export const mdxText = z.string().min(10);
+export const summary = z
+  .string()
+  .min(10)
+  .max(90)
+  .describe(
+    "should not exceed 90 characters, otherwise it would look ugly on the card",
+  );
 export const tags = z.array(z.string().min(1).max(15));
 export const category = z.nativeEnum(PostCategoryEnum);
-export const title = z.string().min(2).max(30);
+export const title = z
+  .string()
+  .min(2)
+  .max(30)
+  .describe("should fit correctly in the card");
