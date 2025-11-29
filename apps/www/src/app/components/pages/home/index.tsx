8af7828ab8b65e481008239b4bd82ba7b2c19773
rccyx <oss@rccyx.com>
2025-11-29 04:06:06 +0100

    copy

--- apps/www/src/app/components/pages/home/index.tsx
+++ apps/www/src/app/components/pages/home/index.tsx
@@ -154,7 +154,7 @@ export function HomePage() {
                     id="hero-title"
                     className="text-4xl -tracking-tight font-display font-medium text-text-strong sm:text-5xl md:text-6xl"
                   >
-                    Modeling reality
+                    Modern renaissance
                     <br />
                     on a binary substrate
                   </h1>
