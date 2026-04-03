63eb7412a01e1a02612e455640e034bf033b18e5
rccyx <oss@rccyx.com>
2026-04-03 18:20:12 +0100

    continuum rename

--- apps/continuum/vitest.config.ts
+++ apps/continuum/vitest.config.ts
@@ -1,8 +1,8 @@
 import path from "path";
-import { defineProject, mergeConfig } from "vitest/config";
+import { defineProject, mergeConfig } from "vitest/config.js";
 
 // importing using @rccyx/vitest-config keep erroring out, and it's annoying af!
-import { baseConfig } from "./../../tooling/vitest";
+import { baseConfig } from "../../tooling/vitest";
 
 export default mergeConfig(
   baseConfig,
